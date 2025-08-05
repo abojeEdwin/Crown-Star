82ec148b9fb12773b724f62feb33fdfebfd02088
Aboje Edwin <abojeedwin@gmail.com>
2025-08-05 09:11:48 +0100

    Creating auth utils

---
+++ utils/jwtTokenUtils.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const generateJwtToken = (userId) => {
+    return jwt.sign({id: userId}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES,
+    });
+};
+
+const verifyJwtToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = {
+    generateJwtToken,
+    verifyJwtToken,
+}
