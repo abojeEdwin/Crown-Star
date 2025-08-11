aa94e94b815a1dd5fcd2ee40c7aa22f2625df8a1
Aboje Edwin <abojeedwin@gmail.com>
2025-08-11 14:52:19 +0100

    Refactored server.js file

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ const playerRoutes = require('./routes/playerRoute');
 const coachRoutes = require('./routes/coachRoute');
 const scoutRoutes = require('./routes/scoutRoute');
 
-// Register routes
-const playerController = require('./controller/playerController');
-const coachController = require('./controller/coachController');
-const scoutController = require('./controller/scoutController');
-
-app.use('/api/player', playerRoutes(playerController));
-app.use('/api/coach', coachRoutes(coachController));
-app.use('/api/scout', scoutRoutes(scoutController));
-app.use(express.urlencoded({ extended: true }));
 
 
 // Initialize express
