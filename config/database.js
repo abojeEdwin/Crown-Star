d777328e3b7528acc1d411d6e7f6ce8e8294c8e1
Aboje Edwin <abojeedwin@gmail.com>
2025-08-11 15:34:27 +0100

    Intergrating Aiven database for deployment

--- config/database.js
+++ config/database.js
@@ -1,11 +1,27 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+// db.js
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: true
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: {
+                ca: fs.readFileSync(__dirname + "/certs/ca.pem"),
+            },
+        },
+        logging: false,
+    }
+);
+
+module.exports = sequelize;
+
 
 const models = require('../models');
 
