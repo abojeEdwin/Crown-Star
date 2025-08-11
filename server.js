9dbd9809b7e6454641ecb110d8450d935d174815
Aboje Edwin <abojeedwin@gmail.com>
2025-08-11 14:50:56 +0100

    Refactored server.js file

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const playerRoutes = require('./routes/playerRoute');
 const coachRoutes = require('./routes/coachRoute');
 const scoutRoutes = require('./routes/scoutRoute');
 
+// Register routes
+const playerController = require('./controller/playerController');
+const coachController = require('./controller/coachController');
+const scoutController = require('./controller/scoutController');
+
+app.use('/api/player', playerRoutes(playerController));
+app.use('/api/coach', coachRoutes(coachController));
+app.use('/api/scout', scoutRoutes(scoutController));
+app.use(express.urlencoded({ extended: true }));
+
+
 // Initialize express
 const app = express();
 const server=http.createServer(app);
@@ -23,47 +34,30 @@ const io = new Server(server, {
  });
  io.on('connection', (socket) => {
      console.log('User connected:', socket.id);
+     socket.on('sendMessage',async (data)  => {
+         try{
+             const message = await Message.create({
+                 senderId: data.senderId,
+                 receiverId: data.receiverId,
+                 content: data.content,
+                 chatId: data.chatId,
+                 read: false,
+             });
+             io.emit('receiveMessage', message);
+         }catch(err){
+             console.error(err);
+         }
+     });
      socket.on('disconnect', () => {
-         console.log('User disconnected:', socket.id);
+        console.log('User disconnected:', socket.id);
      });
  });
- server.listen(3000, () => console.log('Server running on port 3000'));
-
-
-
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-    socket.on('sendMessage', async (data) => {
-        try {
-            const message = await Message.create({
-                senderId: data.senderId,
-                receiverId: data.receiverId,
-                content: data.content,
-                chatId: data.chatId,
-                read: data.read = false,
-            });
-            io.emit('receiveMessage', message);
-        } catch (err) {
-            console.error(err);
-        }
-    });
-});
-
-// Register routes
-const playerController = require('./controller/playerController');
-const coachController = require('./controller/coachController');
-const scoutController = require('./controller/scoutController');
-
-app.use('/api/player', playerRoutes(playerController));
-app.use('/api/coach', coachRoutes(coachController));
-app.use('/api/scout', scoutRoutes(scoutController));
-app.use(express.urlencoded({ extended: true }));
 
 // Health check
 app.get('/', (_req, res) => res.send('🚀 Task Manager API is running...'));
 
 // Start server and DB
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 sequelize.authenticate()
     .then(() => {
         console.log('✅ Connected to Mysql server');
