7cb1237718538c762c1d41529c3e929f91827c6d
Aboje Edwin <abojeedwin@gmail.com>
2025-08-21 15:32:51 +0100

    updating the server.js file to include cors settings

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const corsOptions = {
         }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
 };
 
 app.use(cors(corsOptions));
